ae04ee747fcb54c3fad95ec96e730edbd7725dbf
Lucy M Golcher <luciamongeg@gmail.com>
2019-10-10 22:49:29 -0600

    Casi listo

--- TallerDrRacket.rkt
+++ TallerDrRacket.rkt
@@ -5,54 +5,60 @@
 (define (Fac n)
   (cond ((= n 0)1)
         ((= n 1 )1)
-        ((= n 2 )2)
+        ((= n 2 )2);casos base
         (else (auxFac n))))
 
 (define (auxFac n)
   (* n (Fac(- n 1))))
+;se calcula el factorial de un numero y devulve un numero entero
 
 ;Fibonacci
 (define (Fib n)
   (cond ((= n 0)0)
-        ((= n 1 )1)
+        ((= n 1 )1);casos base
         (else (auxFib n))))
 
 (define (auxFib n)
   (+ (Fib (- n 1 ))(Fib (- n 2))))
+;se calcula el fibonacci de un numero y devulve un numero entero
 
 
 ;Elemento en una lista
 (define (miembro lista ele)
-  (cond ((null? lista)#f)
-        ((equal? ele (car lista))#t)
-  (else (miembro (cdr lista) ele))))
+  (cond ((null? lista)#f);verifica que la lista no este vacia
+        ((equal? ele (car lista))#t);encuentra el elemento
+  (else (miembro (cdr lista) ele))));continua buscando
 
   
 ;Eliminar un elemento de una lista?
 (define(eliminar lista ele)
-  (cond ((null? lista)lista)
-        ((= ele (car lista)) (eliminar (cdr lista) ele))
-        (else (cons (car lista)(eliminar (cdr lista) ele)))))
+  (cond ((null? lista)lista);verifica que la lista no este vacia
+        ((= ele (car lista)) (eliminar (cdr lista) ele));encuentra el elemento, y lo quita de la lista
+        (else (cons (car lista)(eliminar (cdr lista) ele)))));en caso contrario continua buscando
 
 
 ;Quicksort
 (define (QuickS lista)
   (cond ((null? lista) '())
-        (else (append (QuickS (Qmenores (car lista) (cdr lista))) (list (car lista)) (QuickS (Qmayores (car lista) (cdr lista)))))))
+        (else (append (QuickS (Qmenores (car lista) (cdr lista))) (list (car lista)) (QuickS (Qmayores (car lista) (cdr lista))))))); se hace una lista con los resultados de la recursion
 
+;funcion auxiliar para verificar que la lista no esta vacia
 (define (Qmenores pivote lista)
   (cond ((null? lista)'())
         (else (menAux pivote lista '()))))
 
+;funcion para encontrar los menores de un determinado numero
 (define (menAux pivote lista resultado)
   (cond ((null? lista) resultado)
         ((> pivote (car lista)) (menAux pivote (cdr lista) (append resultado (list (car lista)))))
         (else (menAux pivote (cdr lista) resultado))))
 
+;funcion auxiliar para verificar que la lista no esta vacia
 (define (Qmayores pivote lista)
   (cond ((null? lista) '())
         (else (mayAux pivote lista '()))))  
 
+;funcion para encontrar los mayores de un determinado numero
 (define (mayAux pivote lista resultado)
   (cond ((null? lista) resultado)
         ((< pivote (car lista)) (mayAux pivote (cdr lista) (append resultado (list (car lista)))))
@@ -75,50 +81,46 @@
   (cons ((null? arbol) '())
         (else ((elArbolAux nodo arbol '())))))
 
+;funcion auxiliar que recorre el arbol, en busca del nodo por eliminar
 (define (elArbolAux nodo arbol resultado)
-  (cons ((equal? nodo (car arbol)) (append(eliminarAux nodo arbol));encontrado
-        ((< nodo (car arbol))(elArbol nodo (cadr arbol)));se le pasa el lado izq
-        (else(elArbol nodo (caddr arbol)))));se le pasa el lado der
+  (cons ((equal? nodo (car arbol)) (eliminarAux nodo arbol resultado));nodo encontrado, solo se elimina dependiendo del caso
+        ((< nodo (car arbol)) (append (list (car arbol)) (list eliminarAux nodo (cadr arbol)) (list caddr arbol)));se le pasa el lado izquierdo para eliminar segun el caso y se mantiene lo demas
+        (else (append (list (car arbol)) (list (cadr arbol)) (list eliminarAux nodo (caddr arbol))))));se le pasa el lado derecho para eliminar segun el caso y se mantiene lo demas
 
+;funcion para determinar si tiene un hijo izquierdo
 (define (HijoIzq raiz)
   (cond ((equal? raiz '())#f)
         ((null? (car raiz))#f)
         (else #t)))
-  
+
+;funcion para determinar si tiene un hijo derecgo
 (define (HijoDer raiz)
   (cond ((equal? raiz '())#f)
         ((null? (cdar raiz))#f)
         (else #t)))
 
-(define (eliminarAux nodo arbol)
-  (cond (((equal? (HijoIzq (cdr arbol))#f) and (equal? (HijoDer (cdr arbol))#f))
-         (casoUno nodo arbol))   
-        (((equal? (HijoIzq (cdr arbol))#t) and (equal? (HijoDer (cdr arbol))#f))
-         (casoDos nodo arbol))
-        (((equal? (HijoIzq (cdr arbol))#f) and (equal? (HijoDer (cdr arbol))#t))
-         (casoDos nodo arbol))
-        (((equal? (HijoIzq (cdr arbol))#t) and (equal? (HijoDer (cdr arbol))#t))
-         (casoTres nodo arbol))
-        (else #f)))
+;funcion para determinar los casos de eliminacion
+(define (eliminarAux nodo arbol resultado)
+  (cond ((and (equal? (HijoIzq (cdr arbol))#f) (equal? (HijoDer (cdr arbol))#f));caso uno, ningun hijo
+         resultado)
+        
+        ((and (equal? (HijoIzq (cdr arbol))#t) (equal? (HijoDer (cdr arbol))#f)) (append (list (cadr arbol))));caso dos, un hijo izquierdo
+         
+        ((and (equal? (HijoIzq (cdr arbol))#f) (equal? (HijoDer (cdr arbol))#t)) (append (list (caddr arbol)));caso dos, un hijo derecho
+         
+        ((and (equal? (HijoIzq (cdr arbol))#t) (equal? (HijoDer (cdr arbol))#t)) (append (list (encontrarMenor (caddr arbol))) (list (cadr arbol)) (elArbol (encontrarMenor (caddr arbol))(caddr arbol))))) ;caso tres, ambos hijos
 
-(define (casoUno nodo arbol)
-  (cond (())))
-
-(define (casoDos nodo arbol)
-  (cond (())))
-
-(define (casoTres nodo arbol)
-  (cond (())))
+        (else resultado)))
 
 
+;encuentra el menor de los mayores para sustituir en el nodo que se va a eliminar
+(define (encontrarMenor arbol)
+  (cond ((null? arbol)  (list arbol))
+        ((null? (cadr arbol))arbol)
+        (else (encontrarMenor (cadr arbol)))))
 
 
-
-
-(define (eliminarA nodo arbol)
   
 
-;Rutas anchura primero
-;(define (Panchura grafo inicio))
 
 
